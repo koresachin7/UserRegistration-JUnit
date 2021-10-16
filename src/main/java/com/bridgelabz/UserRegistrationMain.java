26876bc21133a96c4b482c4f4a870b83d7641ab3
koresachin7 <koresachin7@gmail.com>
2021-10-16 09:16:08 +0530

    Added: - use the lambda Function

---
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        UserRegistrationImp userRegistration = new UserRegistrationImp();
+        userRegistration.validateUserDetails();
+    }
+}
